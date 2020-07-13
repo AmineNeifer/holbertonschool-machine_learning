7c2ed09895e34d29012d9e9b0b60cfab381c4a25
AmineNeifer <amineneifer2000@gmail.com>
2020-07-13 16:23:37 +0100

    pycodeestyle

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -5,9 +5,9 @@ def matrix_shape(matrix):
     """ calculates the shape of matrix"""
     try:
         return([len(matrix), len(matrix[0]), len(matrix[0][0])])
-    except:
+    except TypeError:
         pass
     try:
         return([len(matrix), len(matrix[0])])
-    except:
+    except TypeError:
         return([len(matrix)])
