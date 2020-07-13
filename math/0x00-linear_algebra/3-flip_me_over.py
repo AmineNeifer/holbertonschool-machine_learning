3d8430fb13b0a49e57dd00e77e26e7cc14f56fac
AmineNeifer <amineneifer2000@gmail.com>
2020-07-13 16:23:56 +0100

    transpose a matrix manually

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def matrix_transpose(matrix):
+    """ returns transpose of a matrix"""
+    new = []
+    for el in matrix[0]:
+        new.append([])
+    for i in range(len(matrix[0])):
+        for j in range(len(matrix)):
+            new[i].append(matrix[j][i])
+    return(new)
