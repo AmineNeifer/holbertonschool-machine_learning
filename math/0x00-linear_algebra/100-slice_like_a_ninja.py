9f4957c6f48785129b53ab8d94fc8d354966830a
AmineNeifer <amineneifer2000@gmail.com>
2020-07-16 16:55:18 +0100

    task 15

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" contains a function that slices a matrix along
+    a specific axe"""
+
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    ax = []
+    i = 0
+    for number, element in axes.items():
+        while i != number:
+            i += 1
+            ax.append(slice(None))
+        i += 1
+        ax.append(slice(*element))
+    ax = tuple(ax)
+    return matrix[ax]
