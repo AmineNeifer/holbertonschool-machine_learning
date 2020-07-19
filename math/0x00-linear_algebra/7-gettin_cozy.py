357c2b1008fced9b2c5202ded8c8952471e3d680
AmineNeifer <amineneifer2000@gmail.com>
2020-07-19 22:32:14 +0100

    output for dimensions don't match

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -8,9 +8,13 @@ def cat_matrices2D(mat1, mat2, axis=0):
     for el in mat1:
         new.append(el[:])
     if axis == 0:
+        if len(mat1) != len(mat2):
+            return None
         for el in mat2:
             new.append(el)
     elif axis == 1:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         for i in range(len(mat2)):
             for el in (mat2[i]):
                 new[i] += [el]
