702868c609e8151b7063159b07cac4c3fef9a33a
AmineNeifer <amineneifer2000@gmail.com>
2020-07-25 00:21:23 +0100

    made it possible for positive and negative n

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -8,7 +8,9 @@ def summation_i_squared(n):
     if type(n) is not int:
         return None
 
-    if n in (0, 1):
-        return n
-
-    return n ** 2 + summation_i_squared(n-1)
+    if n == 1:
+        return 1
+    if n < 0:
+        return n ** 2 + summation_i_squared(n+1)
+    else:
+        return n ** 2 + summation_i_squared(n-1)
