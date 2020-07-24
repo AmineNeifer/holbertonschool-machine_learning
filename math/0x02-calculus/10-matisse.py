95e06f16ecab8b5ccc576bf4379e00fae3f11152
AmineNeifer <amineneifer2000@gmail.com>
2020-07-24 23:43:49 +0100

    pycodestyle and made a clone of the poly

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+
 """ contains a function for deriv"""
+
 
 def poly_derivative(poly):
     """ polynome derivative function"""
@@ -8,7 +10,8 @@ def poly_derivative(poly):
         return None
     if len(poly) == 1:
         return [0]
-    for i in range(len(poly)):
-        poly[i] *= i
-    poly.pop(0)
-    return poly
+    poly_clone = poly[:]
+    for i in range(len(poly_clone)):
+        poly_clone[i] *= i
+    poly_clone.pop(0)
+    return poly_clone
