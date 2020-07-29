c891bb01af5e5ede51a0d8e14c82c6383896477b
AmineNeifer <amineneifer2000@gmail.com>
2020-07-29 16:28:35 +0100

    class poisson

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+""" contains the Poisson distribution Class"""
+
+
+class Poisson:
+    """ Poisson distribution Class"""
+
+    def __init__(self, data=None, lambtha=1.):
+        self.data = data
+        if data is None:
+            if lambtha < 0:
+                raise ValueError("data must be a list")
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 3:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = sum(data) / len(data)
