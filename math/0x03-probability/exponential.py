2e44eb76c2195e25bb2da38cdf7e6995a4d7f2a0
AmineNeifer <amineneifer2000@gmail.com>
2020-07-29 18:02:34 +0100

    expo class

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+""" contains the class Exponential"""
+
+
+class Exponential:
+    """ Exponential class in probability"""
+
+    def __init__(self, data=None, lambtha=1.):
+        self.data = data
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 3:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = len(data) / sum(data)
