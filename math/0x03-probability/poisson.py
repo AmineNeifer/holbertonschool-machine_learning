4fee78a873bb348cdf74fd4878d51d17a6d2b51b
AmineNeifer <amineneifer2000@gmail.com>
2020-07-29 17:08:14 +0100

    poisson pmf funct

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -6,7 +6,6 @@
 
 class Poisson:
     """ Poisson distribution Class"""
-
     def __init__(self, data=None, lambtha=1.):
         self.data = data
         if data is None:
@@ -20,3 +19,17 @@ class Poisson:
             if len(data) < 3:
                 raise ValueError("data must contain multiple values")
             self.lambtha = sum(data) / len(data)
+
+    def factor(self, n):
+        """ calculates the factorial of n"""
+        fac = 1
+        for i in range(1, n+1):
+            fac *= i
+        return fac
+
+    def pmf(self, k):
+        """ calculates the pmf for a given number of successes """
+        e = 2.7182818285
+        lamda = self.lambtha
+        k = int(k)
+        return lamda ** k * e ** (-lamda) / self.factor(k)
