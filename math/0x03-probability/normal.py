030d0276288656648b0da4ea28fc3ce4f3e16dda
AmineNeifer <amineneifer2000@gmail.com>
2020-07-30 17:36:30 +0100

    trying

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -37,7 +37,10 @@ class Normal:
         e = 2.7182818285
         pi = 3.1415926536
         o = self.stddev
-        return e ** ((-1/2) * self.z_score(x) ** 2) / (o * self.sqrt(2 * pi))
+        """return e ** ((-1/2) * self.z_score(x) ** 2)
+        / (o * self.sqrt(2 * pi))"""
+        p = self.z_score(x) ** 2
+        return (1 / (self.stddev * ((2 * pi)**0.5)) * (e ** ((-0.5) * p)))
 
     def cdf(self, x):
         """ Calculates the value of the CDF for a given x-value"""
