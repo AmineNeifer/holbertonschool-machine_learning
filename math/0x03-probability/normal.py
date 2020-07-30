c9b16572d7e8a7be1a21735a4ff4bdb4320f2833
AmineNeifer <amineneifer2000@gmail.com>
2020-07-30 16:04:04 +0100

    initialized self.data

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -6,11 +6,13 @@
 
 class Normal:
     """ normal distibution class"""
+
     def __init__(self, data=None, mean=0., stddev=1.):
+        self.data = data
         if data is None:
             self.stddev = float(stddev)
             self.mean = float(mean)
-            if stddev < 0:
+            if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
         else:
             if type(data) is not list:
