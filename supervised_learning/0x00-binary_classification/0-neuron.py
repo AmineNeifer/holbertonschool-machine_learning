1377a271086628a15e940625a4681c86a1ab25b9
AmineNeifer <amineneifer2000@gmail.com>
2020-08-05 10:34:15 +0100

    initiatin on machine learning

---
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+""" contains class Neuron definition"""
+import numpy as np
+
+
+class Neuron:
+    """ Class Neuron"""
+
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
