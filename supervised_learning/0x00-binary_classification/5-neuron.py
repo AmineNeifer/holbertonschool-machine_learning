2c70fbd06d6ca13fe65ef21e1af1ef5db830a010
AmineNeifer <amineneifer2000@gmail.com>
2020-08-06 21:07:11 +0100

    gradient descent

---
+++ supervised_learning/0x00-binary_classification/5-neuron.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+
+""" contains class Neuron definition"""
+import numpy as np
+
+
+class Neuron:
+    """ Class Neuron"""
+
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """ Calculates the forward propagation which is X.W + b (b = 0)"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """ Calculates the cost"""
+        c = - (np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)))
+        c /= Y.shape[1]
+        return c
+
+    def evaluate(self, X, Y):
+        """ Evaluates the neuron’s predictions"""
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        return np.where(A >= 0.5, 1, 0), cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """ Calculates one pass of gradient descent on the neuron"""
+        m = X.shape[1]
+        A = self.forward_prop(X)
+        dZ = A - Y
+        dW = np.sum(X * dZ, axis=1)
+        db = np.sum(dZ)
+        dW /= m
+        db /= mga
+        self.__W = self.__W - (alpha * dW)
+        self.__b = self.__b - (alpha * db)
