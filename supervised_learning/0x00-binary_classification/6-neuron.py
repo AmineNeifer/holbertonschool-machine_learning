e46edbdb2421cfa144f1376644ad72d2406a1923
AmineNeifer <amineneifer2000@gmail.com>
2020-08-08 10:27:55 +0100

    work on iterations

--- supervised_learning/0x00-binary_classification/6-neuron.py
+++ supervised_learning/0x00-binary_classification/6-neuron.py
@@ -68,7 +68,7 @@ class Neuron:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-        for i in range(5000):
+        for i in range(iterations):
             self.forward_prop(X)
             self.gradient_descent(X, Y, self.__A, alpha)
         return self.evaluate(X, Y)
