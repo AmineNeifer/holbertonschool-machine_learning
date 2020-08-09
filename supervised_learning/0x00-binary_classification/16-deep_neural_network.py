b3727d2768ce9d59b9c5d87d7ac98cae1beaf845
AmineNeifer <amineneifer2000@gmail.com>
2020-08-09 19:11:01 +0100

    removed the first for loop

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -15,9 +15,8 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if type(layers) is not list:
             raise TypeError("layers must be a list of positive integers")
-        for element in layers:
-            if element <= 0:
-                raise TypeError("layers must be a list of positive integers")
+        if (np.array(layers).any() < 0) is False:
+            raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
