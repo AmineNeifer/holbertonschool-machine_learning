859672f000d772014cece05a38a6f6ac96585a4a
AmineNeifer <amineneifer2000@gmail.com>
2020-08-09 20:59:37 +0100

    privatized class

---
+++ supervised_learning/0x00-binary_classification/17-deep_neural_network.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+
+""" contains DeepNeuralNetwork definition"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Class DeepNeuralNetwork"""
+
+    def __init__(self, nx, layers):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or not layers:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(len(layers)):
+            if layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            if i == 0:
+                self.__weights['W' + str(i + 1)
+                             ] = np.random.randn(layers[i], nx) * np.sqrt(2/nx)
+            else:
+                self.__weights['W' + str(i + 1)] = \
+                    np.random.randn(layers[i], layers[i-1]) * \
+                    np.sqrt(2/layers[i-1])
+            self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+    
+    @property
+    def weights(self):
+        return self.__weights
