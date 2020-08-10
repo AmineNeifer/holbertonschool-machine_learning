c56a946946796db33b6f1e302ff4c19a6baf723a
AmineNeifer <amineneifer2000@gmail.com>
2020-08-10 18:05:22 +0100

    added edge cases

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -6,6 +6,13 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """ converts a numeric label vector into a one-hot matrix"""
+    if Y is None or classes is None:
+        return None
+    if type(classes) is not int:
+        return None
+    if classes <= 0:
+        return None
+
     new = np.zeros((Y.shape[0], classes))
     for i in range(len(Y)):
         new[i][Y[i]] = 1
