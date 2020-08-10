48724eaeb5c9a21d86fd9e32e44578ca23288d37
AmineNeifer <amineneifer2000@gmail.com>
2020-08-10 18:08:04 +0100

    added some edge cases

---
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """ that converts a one-hot matrix into a vector of labels"""
+    if one_hot is None:
+        return None
+    m = one_hot.shape[0]
+    new = np.ndarray((m,), dtype=int)
+    try:
+        for i in ange(m):
+            new[i] = int(np.argmax(one_hot.T[:][i]))
+    except IndexError:
+        return None
+    return new
