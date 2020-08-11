9c2e16e85072c13555020a616cf549ab40135250
AmineNeifer <amineneifer2000@gmail.com>
2020-08-11 10:25:23 +0100

    the answer was easy all along

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -7,10 +7,4 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """ that converts a one-hot matrix into a vector of labels"""
-    if one_hot is None:
-        return None
-    m = one_hot.shape[0]
-    new = np.ndarray((m,), dtype=int)
-    for i in range(m):
-        new[i] = int(np.argmax(one_hot.T[:][i]))
-    return new
+    return np.argmax(one_hot, axis=0)
