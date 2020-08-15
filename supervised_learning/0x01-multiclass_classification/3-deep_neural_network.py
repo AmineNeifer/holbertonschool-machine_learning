4ff02b4835c21bad768ebab2475c22e42aa68f52
AmineNeifer <amineneifer2000@gmail.com>
2020-08-15 15:45:30 +0100

    trying

--- supervised_learning/0x01-multiclass_classification/3-deep_neural_network.py
+++ supervised_learning/0x01-multiclass_classification/3-deep_neural_network.py
@@ -113,7 +113,7 @@ class DeepNeuralNetwork:
             if step <= 0 or step > iterations:
                 raise ValueError("step must be positive and <= iterations")
 
-        for i in range(0, iterations + 1):
+        for i in range(0, iterations):
             A, cache = self.forward_prop(X)
             self.gradient_descent(Y, cache, alpha)
             if (i == 0 or i % step == 0 or i == iterations):
@@ -122,11 +122,11 @@ class DeepNeuralNetwork:
                 co.append(cost)
                 if verbose is True:
                     print("Cost after {} iterations: {}".format(i, cost))
-        else:
-            for i in range(0, iterations):
-                A, cache = self.forward_prop(X)
-                self.gradient_descent(Y, cache, alpha)
-                cost = self.cost(Y, A)
+        if verbose is True:
+            cost = self.cost(Y, A)
+            it.append(i)
+            co.append(cost)
+            print("Cost after {} iterations: {}".format(iterations, cost))
         if graph is True:
             plt.plot(it, co)
             plt.xlabel("iteration")
