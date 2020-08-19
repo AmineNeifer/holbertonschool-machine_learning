318cc4a7befd015bc3c544784f66a9fcbdde2bb2
AmineNeifer <amineneifer2000@gmail.com>
2020-08-19 11:46:42 +0000

    idk

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -8,6 +8,6 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """ accuracy count funct"""
-    equality = tf.equal(y_pred, y)
+    equality = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
     return accuracy
