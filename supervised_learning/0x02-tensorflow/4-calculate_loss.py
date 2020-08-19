b290243b94178a74a9b61c2faec391e8ec67d762
AmineNeifer <amineneifer2000@gmail.com>
2020-08-19 12:16:40 +0000

    should be good now

--- supervised_learning/0x02-tensorflow/4-calculate_loss.py
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -8,4 +8,4 @@ import tensorflow as tf
 
 def calculate_loss(y, y_pred):
     """ loss count funct"""
-    return tf.losses.softmax_cross_entropy(y, y_pred) 
+    return tf.losses.softmax_cross_entropy(y, y_pred)
