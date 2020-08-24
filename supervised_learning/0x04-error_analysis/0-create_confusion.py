ec15698fbf559f5c32a22aab5e21dcea16450eb7
AmineNeifer <amineneifer2000@gmail.com>
2020-08-24 15:51:45 +0000

    create confusion matrix

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" contains func create_confusion_matrix"""
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """ creates confuion matrix"""
+    classes = logits.shape[1]
+    m = logits.shape[0]
+    conf_matrix = np.zeros((classes, classes))
+    for i in range(m):
+        print(np.where(labels[i, :] == 1))
+        conf_matrix[np.where(labels[i, :] == 1),
+                    np.where(logits[i, :] == 1)] += 1
+    return conf_matrix
