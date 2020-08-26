e21cfc23828994859ccd26ba41a2f549e776be7b
AmineNeifer <amineneifer2000@gmail.com>
2020-08-26 15:56:12 +0000

    readme file

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -7,8 +7,8 @@ import numpy as np
 def specificity(confusion):
     """ returns an array containing specificity"""
     diag = np.diagonal(np.fliplr(confusion))
-    sensitivity = diag / confusion.sum(axis=1) #TP / (TP + FN)
-    precision = diag / confusion.sum(axis=0) #TP / (TP + FP)
-    """ specificity ="""                         #TN / (TN + FP)
-    
+    sensitivity = diag / confusion.sum(axis=1)  # TP / (TP + FN)
+    precision = diag / confusion.sum(axis=0)  # TP / (TP + FP)
+    """ specificity = still"""  # TN / (TN + FP)
+
     return
