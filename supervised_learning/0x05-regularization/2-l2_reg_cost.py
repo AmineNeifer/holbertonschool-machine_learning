d9a6bb76a71ff29362e8e600456fcaa6e020792d
AmineNeifer <amineneifer2000@gmail.com>
2020-08-27 12:33:48 +0000

    ?

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -9,6 +9,5 @@ def l2_reg_cost(cost):
     """ Returns: a tensor containing the cost
     of the network accounting for L2 regularization"""
     reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-    reg_constant = 0.01
-    loss = cost + reg_constant * sum(reg_losses)
+    loss = cost + reg_losses
     return loss
