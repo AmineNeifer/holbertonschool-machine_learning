ce67eda6cdb760c05db352382a5d96764a97d788
AmineNeifer <amineneifer2000@gmail.com>
2020-08-27 12:26:12 +0000

    al recommendation is good

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 def l2_reg_cost(cost):
     """ Returns: a tensor containing the cost
     of the network accounting for L2 regularization"""
-     reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-     reg_constant = 0.01
-     loss = cost + reg_constant * sum(reg_losses)
-     return loss
+    reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    reg_constant = 0.01
+    loss = cost + reg_constant * sum(reg_losses)
+    return loss
