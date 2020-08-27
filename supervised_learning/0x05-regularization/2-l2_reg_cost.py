8e9bf3d01a989c895afb5b5908978bbb50908ed3
AmineNeifer <amineneifer2000@gmail.com>
2020-08-27 12:18:44 +0000

    ?

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -9,4 +9,4 @@ import tensorflow as tf
 def l2_reg_cost(cost):
     """ Returns: a tensor containing the cost
     of the network accounting for L2 regularization"""
-    return tf.Variable(cost)
+    return tf.contrib.layers.l2_regularizer(cost)
