407eabfe2e86e3967d4e02e0ef888dfe6526151d
AmineNeifer <amineneifer2000@gmail.com>
2020-08-28 12:43:27 +0000

    ?

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -18,9 +18,9 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         if i == L - 1:
             dz = A - Y
         else:
-            dz = np.matmul(weights['W' + str(i + 2)].T, dz) * (1 - (A * A))
+            dz = np.matmul(weis['W' + str(i + 2)].T, dz) * (1 - (A * A))
         dW = np.matmul(dz, prev_A.T) / m
         db = np.sum(dz, axis=1, keepdims=True) / m
-        weights['W' + str(i + 1)] = weis["W" + str(i + 1)] - \
-            alpha * lambtha * weis['W' + str(i + 1)] / m - alpha * dW
-        weights['b' + str(i + 1)] = weis["b" + str(i + 1)] - (alpha * db)
+        weights['W' + str(i + 1)] = weights["W" + str(i + 1)] - \
+            alpha * lambtha * weights['W' + str(i + 1)] / m - alpha * dW
+        weights['b' + str(i + 1)] = weights["b" + str(i + 1)] - (alpha * db)
