6b1f1a2ccf206bb24bbfab2c4d92d233f96ad1dc
AmineNeifer <amineneifer2000@gmail.com>
2020-08-31 12:42:18 +0000

    added regularizer to all layers

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -8,6 +8,7 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """ build model with keras"""
     list_layer = []
+    l2 = kernel_regularizer = K.regularizers.l2(lambtha)
     for i in range(len(layers)):
         if i < len(layers) - 1:
             if (i == 0):
@@ -18,15 +19,16 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
                         input_shape=(
                             nx,
                         ),
-                        kernel_regularizer=K.regularizers.l2(lambtha)))
+                        kernel_regularizer=l2))
             else:
                 list_layer.append(
                     K.layers.Dense(
                         layers[i],
-                        activation=activations[i]))
+                        activation=activations[i]),
+                    kernel_regularizer=l2)
             list_layer.append(K.layers.Dropout(keep_prob))
         else:
-            list_layer.append(K.layers.Dense(layers[i]))
+            list_layer.append(K.layers.Dense(layers[i], kernel_regularizer=l2))
 
     model = K.Sequential(list_layer)
     return model
