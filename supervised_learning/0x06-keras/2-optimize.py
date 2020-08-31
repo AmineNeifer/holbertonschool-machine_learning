e6fae52eb67d1003939204418675b6e478d2d00d
AmineNeifer <amineneifer2000@gmail.com>
2020-08-31 16:10:39 +0000

    now it works

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -8,6 +8,7 @@ import tensorflow.keras as K
 def optimize_model(network, alpha, beta1, beta2):
     """ optimizes the model"""
     optimizer = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
+
     network.compile(
         optimizer=optimizer,
-        loss=tf.keras.losses.CategoricalCrossentropy())
+        loss=K.losses.categorical_crossentropy)
