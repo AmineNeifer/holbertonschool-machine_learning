8574eac234fadb9b08ec3633cc89581d6034aee0
AmineNeifer <amineneifer2000@gmail.com>
2020-09-01 11:36:08 +0000

    this should do

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -7,22 +7,19 @@ import tensorflow.keras as K
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """ build a model using Input class"""
-    l2 = kernel_regularizer = K.regularizers.l2(lambtha)
     inputs = K.Input(shape=(nx,))
+    l2 = kernel_regularizer = K.regularizers.l2(lambtha)
     dense = K.layers.Dense(
         layers[0],
         activation=activations[0],
         kernel_regularizer=l2)
     x = dense(inputs)
-    x = K.layers.Dropout(keep_prob)(x)
     for i in range(1, len(layers)):
+        x = K.layers.Dropout(1 - keep_prob)(x)
         x = K.layers.Dense(
             layers[i],
             activation=activations[i],
             kernel_regularizer=l2)(x)
-        if (i == len(layers) - 1):
-            break
-        x = K.layers.Dropout(keep_prob)(x)
 
     model = K.Model(inputs=inputs, outputs=x)
     return model
