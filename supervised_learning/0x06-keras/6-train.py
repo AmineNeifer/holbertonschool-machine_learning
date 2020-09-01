76e2656a4feabf9a6cfc03a6b6bea8578e13cdab
AmineNeifer <amineneifer2000@gmail.com>
2020-09-01 13:10:45 +0000

    task 6

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+""" Contains a functin that trains a model"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, validation_data=None,
+                early_stopping=False, patience=0, verbose=True, shuffle=False):
+    """ train model using keras"""
+    if (early_stopping and validation_data):
+        callback = K.callbacks.EarlyStopping(
+            monitor="val_loss", patience=patience)
+    history = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=validation_data,
+        shuffle=shuffle,
+        verbose=verbose,
+        callbacks=[callback])
+    return history
