c162a420fb2efcb41cc9eb3d85607e50b3e58ada
AmineNeifer <amineneifer2000@gmail.com>
2020-09-01 11:46:34 +0000

    edge case

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -8,5 +8,7 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size,
                 epochs, validation_data=None, verbose=True, shuffle=False):
     """ train model using keras"""
+    if len(validation_data) != 2:
+        validation_data = None
     history = network.fit(data, labels, batch_size=batch_size,epochs=epochs, validation_data=validation_data,shuffle=shuffle, verbose=verbose)
     return history
