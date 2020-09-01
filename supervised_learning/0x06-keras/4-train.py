c3d5fa0998226868966cbae9b609f805df9804be
AmineNeifer <amineneifer2000@gmail.com>
2020-09-01 11:40:31 +0000

    pycodetyle + added doc

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -7,6 +7,7 @@ import tensorflow.keras as K
 
 def train_model(network, data, labels, batch_size,
                 epochs, verbose=True, shuffle=False):
+    """ trains a model using keras"""
     history = network.fit(data, labels, batch_size=batch_size,
                           epochs=epochs, shuffle=shuffle, verbose=verbose)
     return history
