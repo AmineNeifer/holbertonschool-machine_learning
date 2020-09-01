ef03713e790e9ebf8f5f260511ce56cb17ec3043
AmineNeifer <amineneifer2000@gmail.com>
2020-09-01 11:45:09 +0000

    that's awkward

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -8,7 +8,5 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size,
                 epochs, validation_data=None, verbose=True, shuffle=False):
     """ train model using keras"""
-    history = network.fit(data, labels, batch_size=batch_size,
-                          epochs=epochs, validation_data=validation_data,
-                          shuffle=shuffle, verbose=verbose)
+    history = network.fit(data, labels, batch_size=batch_size,epochs=epochs, validation_data=validation_data,shuffle=shuffle, verbose=verbose)
     return history
