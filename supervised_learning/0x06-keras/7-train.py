d0daf810ef4e7b82d1ebbca1359c621ab7acf2a9
AmineNeifer <amineneifer2000@gmail.com>
2020-09-01 13:58:49 +0000

    task 7

---
+++ supervised_learning/0x06-keras/7-train.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+""" Contains a functin that trains a model"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, verbose=True, shuffle=False):
+    """ train model using keras, with learning rate decay"""
+    callback = []
+    if (validation_data):
+        if (early_stopping):
+            callback.append(K.callbacks.EarlyStopping(
+                monitor="val_loss", patience=patience))
+        if (learning_rate_decay):
+            def scheduler(epoch):
+                """ step decay func"""
+                return alpha / (1 + decay_rate * epoch)
+            c_back = K.callbacks.LearningRateScheduler(scheduler, 1)
+            callback.append(c_back)
+    else:
+        callback = None
+
+    history = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=validation_data,
+        shuffle=shuffle,
+        verbose=verbose,
+        callbacks=callback)
+    return history
