ed5680d7fec868799717a8f6f1a0d83bf533fc80
AmineNeifer <amineneifer2000@gmail.com>
2020-09-01 11:03:13 +0000

    try like this

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -7,28 +7,16 @@ import tensorflow.keras as K
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """ build model with keras"""
-    list_layer = []
     l2 = K.regularizers.l2(lambtha)
+    model = K.Sequential()
     for i in range(len(layers)):
         if i < len(layers) - 1:
             if (i == 0):
-                list_layer.append(
-                    K.layers.Dense(
-                        layers[i],
-                        activation=activations[i],
-                        input_shape=(
-                            nx,
-                        ),
-                        kernel_regularizer=l2))
+                model.add(K.layers.Dense(layers[i], activation=activations[i], input_shape=( nx,),kernel_regularizer=l2))
             else:
-                list_layer.append(
-                    K.layers.Dense(
-                        layers[i],
-                        activation=activations[i],
-                        kernel_regularizer=l2))
-            list_layer.append(K.layers.Dropout(1 - keep_prob))
+                model.add( K.layers.Dense( layers[i], activation=activations[i], kernel_regularizer=l2))
+            model.add(K.layers.Dropout(1 - keep_prob))
         else:
-            list_layer.append(K.layers.Dense(layers[i], kernel_regularizer=l2))
+            model.add(K.layers.Dense(layers[i], kernel_regularizer=l2))
 
-    model = K.Sequential(list_layer)
     return model
