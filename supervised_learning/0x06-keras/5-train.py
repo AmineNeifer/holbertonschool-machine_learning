c92987425ac1560a55937e3670c1b14613ec577f
AmineNeifer <amineneifer2000@gmail.com>
2020-09-01 11:49:15 +0000

    let's try

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -8,7 +8,7 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size,
                 epochs, validation_data=None, verbose=True, shuffle=False):
     """ train model using keras"""
-    if len(validation_data) != 2:
+    if validation_data is not None and len(validation_data) != 2:
         validation_data = None
     history = network.fit(data, labels, batch_size=batch_size,epochs=epochs, validation_data=validation_data,shuffle=shuffle, verbose=verbose)
     return history
