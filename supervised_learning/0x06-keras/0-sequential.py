c2240ab8bd2685411dd9e8148a4f5e4ea838409f
AmineNeifer <amineneifer2000@gmail.com>
2020-09-01 11:16:55 +0000

    changed architecture

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -9,21 +9,19 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """ build model with keras"""
     l2 = K.regularizers.l2(lambtha)
     model = K.Sequential()
-    for i in range(len(layers)):
+    model.add(K.layers.Dense(
+               layers[0],
+               activation=activations[0],
+               input_shape=(nx,),
+               kernel_regularizer=l2))
+
+    model.add(K.layers.Dropout(1 - keep_prob))
+    for i in range(1, len(layers)):
         if i < len(layers) - 1:
-            if (i == 0):
-                model.add(
-                    K.layers.Dense(
-                        layers[i],
-                        activation=activations[i],
-                        input_shape=nx,
-                        kernel_regularizer=l2))
-            else:
-                model.add(
-                    K.layers.Dense(
-                        layers[i],
-                        activation=activations[i],
-                        kernel_regularizer=l2))
+            model.add(K.layers.Dense(
+                      layers[i],
+                      activation=activations[i],
+                      kernel_regularizer=l2))
             model.add(K.layers.Dropout(1 - keep_prob))
         else:
             model.add(
