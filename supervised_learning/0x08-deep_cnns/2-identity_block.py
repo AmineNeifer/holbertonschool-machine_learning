168da5e74b4df5ac0adaeec5ba1004adc6df8e80
AmineNeifer <amineneifer2000@gmail.com>
2020-09-16 21:47:38 +0000

    put padding='same' in all conv2D s

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -8,7 +8,7 @@ def identity_block(A_prev, filters):
     """ Builds an Identity block"""
     F11, F3, F12 = filters
 
-    conv_1x1 = K.layers.Conv2D(filters=F11, kernel_size=1)(A_prev)
+    conv_1x1 = K.layers.Conv2D(filters=F11, kernel_size=1, padding='same')(A_prev)
     conv_1x1 = K.layers.BatchNormalization()(conv_1x1)
     conv_1x1 = K.layers.Activation('relu')(conv_1x1)
     conv_3x3 = K.layers.Conv2D(
@@ -17,7 +17,7 @@ def identity_block(A_prev, filters):
         padding='same')(conv_1x1)
     conv_3x3 = K.layers.BatchNormalization()(conv_3x3)
     conv_3x3 = K.layers.Activation('relu')(conv_3x3)
-    conv_1x1 = K.layers.Conv2D(filters=F12, kernel_size=1)(conv_3x3)
+    conv_1x1 = K.layers.Conv2D(filters=F12, kernel_size=1, padding='same')(conv_3x3)
     conv_1x1 = K.layers.BatchNormalization()(conv_1x1)
     added = K.layers.Add()([A_prev, conv_1x1])
     output = K.layers.Activation('relu')(added)
