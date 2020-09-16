2b0b69e45ec77d37c9d20ec2284a8a39ab4d7b11
AmineNeifer <amineneifer2000@gmail.com>
2020-09-16 21:55:15 +0000

    forgot the he_normal intialization lol

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -11,19 +11,19 @@ def identity_block(A_prev, filters):
     conv_1x1 = K.layers.Conv2D(
         filters=F11,
         kernel_size=1,
-        padding='same')(A_prev)
+        padding='same', kernel_initializer="he_normal")(A_prev)
     conv_1x1 = K.layers.BatchNormalization()(conv_1x1)
     conv_1x1 = K.layers.Activation('relu')(conv_1x1)
     conv_3x3 = K.layers.Conv2D(
         filters=F3,
         kernel_size=3,
-        padding='same')(conv_1x1)
+        padding='same', kernel_initializer="he_normal")(conv_1x1)
     conv_3x3 = K.layers.BatchNormalization()(conv_3x3)
     conv_3x3 = K.layers.Activation('relu')(conv_3x3)
     conv_1x1 = K.layers.Conv2D(
         filters=F12,
         kernel_size=1,
-        padding='same')(conv_3x3)
+        padding='same', kernel_initializer="he_normal")(conv_3x3)
     conv_1x1 = K.layers.BatchNormalization()(conv_1x1)
     added = K.layers.Add()([conv_1x1, A_prev])
     output = K.layers.Activation('relu')(added)
