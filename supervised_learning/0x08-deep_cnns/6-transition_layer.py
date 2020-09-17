b06bf1aa51dddc110887baa64d3377c1788b79f2
AmineNeifer <amineneifer2000@gmail.com>
2020-09-17 20:40:20 +0000

    transition layer in DenseNet

---
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+""" Contains transitions layer for DenseNet"""
+import tensorflow.keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """ Builds a transition layer"""
+    X = K.layers.BatchNormalization()(X)
+    X = K.layers.Activation('relu')(X)
+    nb_filters = int(nb_filters * compression)
+    X = K.layers.Conv2D(
+        filters=nb_filters,
+        kernel_size=1,
+        padding='same',
+        kernel_initializer='he_normal')(X)
+    X = K.layers.AveragePooling2D(2, strides=2)(X)
+    return X, nb_filters
