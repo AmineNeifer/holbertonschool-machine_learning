a29d36522c0f7a73ffe1849161838e5aa010d224
AmineNeifer <amineneifer2000@gmail.com>
2020-09-24 21:17:39 +0000

    staircase = true

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -8,4 +8,4 @@ import tensorflow as tf
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """ createsd learning rate decay operation in tf"""
     return tf.train.inverse_time_decay(
-        alpha, global_step, decay_step, decay_rate)
+        alpha, global_step, decay_step, decay_rate, staircase=True)
