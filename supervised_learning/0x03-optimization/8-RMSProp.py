acdf75d737fa72ff4f2fc5d7075969b800f99ac7
AmineNeifer <amineneifer2000@gmail.com>
2020-09-24 19:56:39 +0000

    rmsprop using tensorflow

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+""" RMSPROP USING TENSORFLOW"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """ RMSProp"""
+    return tf.train.RMSPropOptimizer(
+        alpha, beta2, epsilon=epsilon).minimize(loss)
