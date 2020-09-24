55bbcfc7a1489ddaaf222cfc4c9a3149ac782974
AmineNeifer <amineneifer2000@gmail.com>
2020-09-24 21:12:23 +0000

    learning rate using tensorflow

---
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+""" learning rate decay tensorflow"""
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """ createsd learning rate decay operation in tf"""
+    return tf.train.inverse_time_decay(
+        alpha, global_step, decay_step, decay_rate)
