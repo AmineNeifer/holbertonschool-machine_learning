002fde08f4f0988f14f1f7fe53924bece2c87ba8
AmineNeifer <amineneifer2000@gmail.com>
2020-09-24 20:13:36 +0000

    adam opt with tensorflow <3

---
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+"""adam optimization with tensorflow"""
+import tensorflow as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """ adam opt"""
+    return tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon).minimize(loss)
