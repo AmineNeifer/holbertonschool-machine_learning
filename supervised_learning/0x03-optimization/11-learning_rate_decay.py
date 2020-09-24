9b451799bac8164a8afc9b946479f638010cf823
AmineNeifer <amineneifer2000@gmail.com>
2020-09-24 21:05:49 +0000

    learning rate decay

---
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+""" learning rate decay"""
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """learning rate decay manually"""
+    return alpha / (1 + decay_rate * (global_step // decay_step))
