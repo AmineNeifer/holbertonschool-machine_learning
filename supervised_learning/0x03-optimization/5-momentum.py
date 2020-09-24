5dbdcc588444f50806fa05635de83db3468413aa
AmineNeifer <amineneifer2000@gmail.com>
2020-09-24 19:17:34 +0000

    changed the names of the variables

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -5,6 +5,6 @@
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """ updates a variable using th egradient descent of momentum"""
-    v = beta1 * v + (1 - beta1) * grad
-    var = var - alpha * v
-    return var, grad
+    vdw = beta1 * v + (1 - beta1) * grad
+    w = var - alpha * vdw
+    return w, vdw
