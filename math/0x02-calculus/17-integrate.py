51da2f2f92cda70e5d62b54278f4503cd89b163e
AmineNeifer <amineneifer2000@gmail.com>
2020-09-25 13:22:52 +0100

    this should work, if poly contains a list with a single 0, it shouldn't be added to the poly_clone for integration

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -7,9 +7,12 @@ def poly_integral(poly, C=0):
     """ integraling a polynom"""
     if not isinstance(poly, list) or not isinstance(C, int) or (not poly):
         return None
-    poly_clone = poly[:]
+    if poly != [0]:
+        poly_clone = poly[:]
+    else:
+        poly_clone = []
     poly_clone.insert(0, C)
-    for i in range(len(poly_clone)):
+    for i in range(1, len(poly_clone)):
         if (poly_clone[i] != 0):
             poly_clone[i] /= i
             if poly_clone[i] == int(poly_clone[i]):
