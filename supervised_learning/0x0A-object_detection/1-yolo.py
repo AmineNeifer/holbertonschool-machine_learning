1bfa0417a9c7ecc8d54673152db0471d84231a6b
AmineNeifer <amineneifer2000@gmail.com>
2020-10-03 13:55:00 +0000

    should be as clean as ever

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -38,6 +38,8 @@ class Yolo():
 
         img_h = image_size[0]
         img_w = image_size[1]
+        input_h = self.model.input.shape[1].value
+        input_w = self.model.input.shape[2].value
         for i in range(len(outputs)):
 
             anchor = self.anchors[i]
@@ -58,40 +60,18 @@ class Yolo():
             cx = np.indices((grid_h, grid_w, a))[1]
             cy = np.indices((grid_h, grid_w, a))[0]
 
-            bx = self.sigmoid(tx) + cx
-            by = self.sigmoid(ty) + cy
-            bw = anchor_w * np.exp(tw)
-            bh = anchor_h * np.exp(th)
+            bx = (self.sigmoid(tx) + cx) / grid_w
+            by = (self.sigmoid(ty) + cy) / grid_h
+            bw = anchor_w * np.exp(tw) / input_w
+            bh = anchor_h * np.exp(th) / input_h
 
             x1 = bx - bw / 2
             x2 = x1 + bw
             y1 = by - bh / 2
             y2 = y1 + bh
-            boxes[i][..., 0] = x1 * image_size[1]
-            boxes[i][..., 1] = y1 * image_size[0]
-            boxes[i][..., 2] = x2 * image_size[1]
-            boxes[i][..., 3] = y2 * image_size[0]
+            boxes[i][..., 0] = x1 * img_w
+            boxes[i][..., 1] = y1 * img_h
+            boxes[i][..., 2] = x2 * img_w
+            boxes[i][..., 3] = y2 * img_h
 
         return boxes, box_c, box_c_b
-
-        """ for cy in range(grid_height):
-                for cx in range(grid_width):
-                    for j in range(len(bounding_boxes)):
-                        a = bounding_boxes[j]
-
-                        tx = boxes[i][cx, cy, j, 0]
-                        ty = boxes[i][cx, cy, j, 1]
-                        tw = boxes[i][cx, cy, j, 2]
-                        th = boxes[i][cx, cy, j, 3]
-
-                        bx = (self.sigmoid(tx) + cx) / grid_width
-                        by = (self.sigmoid(ty) + cy) / grid_height
-
-                        bw = a[0] * np.exp(tw) / inp_w
-                        bh = a[1] * np.exp(th) / inp_h
-
-                        boxes[i][cx, cy, j, 0] = (bx - bw / 2) * img_w
-                        boxes[i][cx, cy, j, 1] = (by - bh / 2) * img_h
-                        boxes[i][cx, cy, j, 2] = (bx + bw / 2) * img_w
-                        boxes[i][cx, cy, j, 3] = (by + bh / 2) * img_h
-        """
