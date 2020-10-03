aa7fb5d27bfe8cbab9da9cae6863f044eea0b216
AmineNeifer <amineneifer2000@gmail.com>
2020-10-03 13:50:54 +0000

    let's see

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -25,42 +25,73 @@ class Yolo():
     def process_outputs(self, outputs, image_size):
         """f sogmoid(x):
            return 1 / (1 + np.exp(-x))"""
-        inp_w = int(self.model.input.shape[1])
-        inp_h = int(self.model.input.shape[2])
+        inp_w = self.model.input.shape[1].value
+        inp_h = self.model.input.shape[2].value
 
         boxes = []
         box_c = []
         box_c_b = []
+        for output in outputs:
+            boxes.append(output[..., :4])
+            box_c.append(self.sigmoid(output[..., 4:5]))
+            box_c_b.append(self.sigmoid(output[..., 5:]))
+
         img_h = image_size[0]
         img_w = image_size[1]
         for i in range(len(outputs)):
 
-            output = outputs[i]
-            grid_height = output.shape[0]
-            grid_width = output.shape[1]
+            anchor = self.anchors[i]
+            a = anchor.shape[0]
+            anchor_w = anchor[:, 0]
+            anchor_h = anchor[:, 1]
+            grid_h = boxes[i].shape[0]
+            grid_w = boxes[i].shape[1]
+
+            tx = boxes[i][..., 0]
+            ty = boxes[i][..., 1]
+            tw = boxes[i][..., 2]
+            th = boxes[i][..., 3]
+
             anchor_boxes = self.anchors
-            box = output[..., :4]
-            for cy in range(grid_height):
+            bounding_boxes = anchor_boxes[i]
+
+            cx = np.indices((grid_h, grid_w, a))[1]
+            cy = np.indices((grid_h, grid_w, a))[0]
+
+            bx = self.sigmoid(tx) + cx
+            by = self.sigmoid(ty) + cy
+            bw = anchor_w * np.exp(tw)
+            bh = anchor_h * np.exp(th)
+
+            x1 = bx - bw / 2
+            x2 = x1 + bw
+            y1 = by - bh / 2
+            y2 = y1 + bh
+            boxes[i][..., 0] = x1 * image_size[1]
+            boxes[i][..., 1] = y1 * image_size[0]
+            boxes[i][..., 2] = x2 * image_size[1]
+            boxes[i][..., 3] = y2 * image_size[0]
+
+        return boxes, box_c, box_c_b
+
+        """ for cy in range(grid_height):
                 for cx in range(grid_width):
-                    bounding_boxes = anchor_boxes[i]
                     for j in range(len(bounding_boxes)):
                         a = bounding_boxes[j]
-                        tx = box[cx, cy, j, 0]
-                        ty = box[cx, cy, j, 1]
-                        tw = box[cx, cy, j, 2]
-                        th = box[cx, cy, j, 3]
-                        bx = (self.sigmoid(tx) + cx)
-                        bx /= grid_width
-                        by = (self.sigmoid(ty) + cy) 
-                        by /= grid_height
+
+                        tx = boxes[i][cx, cy, j, 0]
+                        ty = boxes[i][cx, cy, j, 1]
+                        tw = boxes[i][cx, cy, j, 2]
+                        th = boxes[i][cx, cy, j, 3]
+
+                        bx = (self.sigmoid(tx) + cx) / grid_width
+                        by = (self.sigmoid(ty) + cy) / grid_height
 
                         bw = a[0] * np.exp(tw) / inp_w
                         bh = a[1] * np.exp(th) / inp_h
-                        box[cx, cy, j, 0] = (bx - bw / 2) * img_w
-                        box[cx, cy, j, 1] = (by - bh / 2) * img_h
-                        box[cx, cy, j, 2] = (bx + bw / 2) * img_w
-                        box[cx, cy, j, 3] = (by + bh / 2) * img_h
-            boxes.append(box)
-            box_c.append(self.sigmoid(output[:, :, :, 4:5]))
-            box_c_b.append(self.sigmoid(output[:, :, :, 5:]))
-        return boxes, box_c, box_c_b
+
+                        boxes[i][cx, cy, j, 0] = (bx - bw / 2) * img_w
+                        boxes[i][cx, cy, j, 1] = (by - bh / 2) * img_h
+                        boxes[i][cx, cy, j, 2] = (bx + bw / 2) * img_w
+                        boxes[i][cx, cy, j, 3] = (by + bh / 2) * img_h
+        """
