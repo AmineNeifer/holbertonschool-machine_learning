99ceb72a3930fd6260602b185e8ab0965ce53de8
AmineNeifer <amineneifer2000@gmail.com>
2020-10-10 20:01:20 +0000

    wrote doc for init func

--- supervised_learning/0x0A-object_detection/3-yolo.py
+++ supervised_learning/0x0A-object_detection/3-yolo.py
@@ -11,6 +11,7 @@ class Yolo():
     """Class that uses the Yolo v3 algorithm to perform object detection"""
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """initialization"""
         self.model = K.models.load_model(model_path)
         with open(classes_path, "r") as f:
             r = f.read().split("\n")[:-1]
@@ -91,10 +92,10 @@ class Yolo():
         x2, y2, w2, h2 = box2
         w_intersection = min(x1 + w1, x2 + w2) - max(x1, x2)
         h_intersection = min(y1 + h1, y2 + h2) - max(y1, y2)
-        if w_intersection <= 0 or h_intersection <= 0: # No overlap
+        if w_intersection <= 0 or h_intersection <= 0:  # No overlap
             return 0
         I = w_intersection * h_intersection
-        U = w1 * h1 + w2 * h2 - I # Union = Total Area - I
+        U = w1 * h1 + w2 * h2 - I  # Union = Total Area - I
         return I / U
 
     def non_max_suppression(self, filtered_boxes, box_classes, box_scores):
