a7c27a1033010b1e2933025982ed477f92adcaea
AmineNeifer <amineneifer2000@gmail.com>
2020-10-22 14:12:38 +0000

    minor of matrix

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
+
+""" doing the minor of a matrix"""
 det = __import__('0-determinant').determinant
+
+
 def minor(matrix):
+    """ takes a matrix and returns it's minor"""
+    if not matrix:
+        raise TypeError("matrix must be a list of lists")
+    elif len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+    if len(matrix) == 1:
+        return [[1]]
+
     mat = [m[:] for m in matrix[:]]
     for i in range(len(mat)):
         for j in range(len(mat[0])):
-            print(rest(mat, i, j))
-    return (1)
+            mat[i][j] = det(rest(matrix, i, j))
+    return mat
+
 
 def rest(m, i, j):
     """
