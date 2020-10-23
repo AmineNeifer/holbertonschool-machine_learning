dac38ccb9c7f796805e577968da7748dde455fa3
AmineNeifer <amineneifer2000@gmail.com>
2020-10-23 17:33:11 +0000

    should work, task 5

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+""" contains definiteness funct"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """ returns the definitenetss of a matrix"""
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+    if matrix.size == 0:
+        return None
+    if matrix.shape[0] != matrix.shape[1]:
+        return None
+    x = matrix
+    if np.all(np.linalg.eigvals(x) > 0):
+        return "Positive definite"
+    elif np.all(np.linalg.eigvals(x) >= 0):
+        return "Positive semi-definite"
+    elif np.all(np.linalg.eigvals(x) < 0):
+        return "Negative definite"
+    elif np.all(np.linalg.eigvals(x) <= 0):
+        return "Negative semi-definite"
+    # elif np.all(np.linalg.eigvals(x) == 0):
+    else:
+        return "Indifinite"
