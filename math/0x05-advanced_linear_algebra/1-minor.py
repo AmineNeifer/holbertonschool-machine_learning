625059ef306856a9e80bf204ec89cbdfd9db2e40
AmineNeifer <amineneifer2000@gmail.com>
2020-10-23 15:26:44 +0000

    removed the importing line

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -2,7 +2,6 @@
 
 
 """ doing the minor of a matrix"""
-det = __import__('0-determinant').determinant
 
 
 def minor(matrix):
@@ -19,6 +18,40 @@ def minor(matrix):
         for j in range(len(mat[0])):
             mat[i][j] = det(rest(matrix, i, j))
     return mat
+
+
+def determinant(matrix):
+    """ return the determinant of matrix"""
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a list of lists")
+    if not matrix:
+        raise TypeError("matrix must be a list of lists")
+    for item in matrix:
+        if not isinstance(item, list):
+            raise TypeError("matrix must be a list of lists")
+    if matrix == [[]]:
+        return 1
+    elif len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+
+    mat = [a[:] for a in matrix[:]]
+    return det_helper(mat)
+
+
+def det_helper(mat):
+    """ helper for deteminant, is the recursive funct"""
+    if len(mat) == 1:
+        return mat[0][0]
+    if len(mat) == 2:
+        return mat[0][0] * mat[1][1] - mat[1][0] * mat[0][1]
+    det = 0
+    a = -1
+    for i in range(len(mat)):
+        a *= -1
+        m = [part[:] for part in mat[:]]
+        r = rest(m, 0, i)
+        det += a * m[0][i] * det_helper(r)
+    return det
 
 
 def rest(m, i, j):
