2e68d42e8e468203ddd8378bfd86876a3e50734c
AmineNeifer <amineneifer2000@gmail.com>
2020-10-23 15:43:23 +0000

    i took it back to what it was before the maybes

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -16,10 +16,6 @@ def determinant(matrix):
         return 1
     elif len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
-    for item in matrix:
-        for val in item:
-            if not isinstance(val, int) or not isinstance(val, float):
-                raise ValueError("matrix must be a square matrix")
 
     mat = [a[:] for a in matrix[:]]
     return det_helper(mat)
