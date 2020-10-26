a4d9eaec093e0aaf4c1c4cab6352814acd95bf75
AmineNeifer <amineneifer2000@gmail.com>
2020-10-26 21:46:32 +0000

    maybe ?

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -19,6 +19,10 @@ class MultiNormal:
 
     def pdf(self, x):
         """ pdf at a data point"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        if len(x.shape) != 2 or x.shape[1] != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
         d = x.shape[0]
         m = self.mean
         E = np.linalg.det(self.cov)
