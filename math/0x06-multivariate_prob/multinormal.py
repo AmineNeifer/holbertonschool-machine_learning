daefb5ba66a8f8ac6a43c143f09325ccb2eca9f8
AmineNeifer <amineneifer2000@gmail.com>
2020-10-26 19:19:24 +0000

    no use of np.cov

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -13,4 +13,5 @@ class MultiNormal:
         if data.shape[1] < 2:
             raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1, keepdims=True)
-        self.cov = np.cov(data)
+        self.cov = np.matmul((data - self.mean),
+                             (data - self.mean).T) / (data.shape[1] - 1)
