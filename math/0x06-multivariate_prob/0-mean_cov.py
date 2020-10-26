e53fffbeb6426a953dc30274330f89d9f52f82c4
AmineNeifer <amineneifer2000@gmail.com>
2020-10-26 18:39:24 +0000

    lol that wasn't hard but it was unexpected

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" contains a function that calculates both mean and covariance"""
+import numpy as np
+
+
+def mean_cov(X):
+    """ returns mean and covariance"""
+    if not isinstance(X, np.ndarray):
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+    mean = np.mean(X, axis=0, keepdims=True)
+    return mean, np.matmul((X - mean).T, (X - mean)) / X.shape[0]
