b85793346ee90c63f153cb11d6761c19666d6509
AmineNeifer <amineneifer2000@gmail.com>
2020-10-27 21:34:36 +0000

    lol

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" pca """
+import numpy as np
+
+def pca(X, ndim):
+    """ pca yehi"""
+    X_m = X - np.mean(X, axis=0)
+    m = X.shape[0]
+    u, s, vh = np.linalg.svd(X_m)
+    v = np.cumsum(s) / np.sum(s)
+    W = vh.T[:, :ndim]
+    T = np.matmul(X_m, W)
+    return T
