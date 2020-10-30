f73c0576a79e8f3860664fe005e6f6c4b96ac8d6
AmineNeifer <amineneifer2000@gmail.com>
2020-10-30 19:15:19 +0000

    try

---
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+
+""" Bayesian prob"""
+import numpy as np
+from math import factorial as fact
+
+
+def comb(n, x):
+    """ combination : nCx"""
+    return fact(n) / (fact(x) * fact(n - x))
+
+
+def likelihood(x, n, P):
+    """ likelihood"""
+    return comb(n, x) * P ** x * (1 - P) ** (n - x)
+
+
+def intersection(x, n, P, Pr):
+    """ intersection"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray):
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not ((0 <= P).all() and (P <= 1).all()):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if not isinstance(Pr, np.ndarray):
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if not ((0 <= Pr).all() and (Pr <= 1).all()):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(Pr.all(), 1):
+        raise ValueError("Pr must sum to 1")
+    return likelihood(x, n, P) * Pr
