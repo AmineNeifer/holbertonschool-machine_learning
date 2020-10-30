d6b3289eba5de9a79474490129434ff1b5bd569f
AmineNeifer <amineneifer2000@gmail.com>
2020-10-30 19:18:01 +0000

    oh ye wedhni

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -33,6 +33,6 @@ def intersection(x, n, P, Pr):
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if not ((0 <= Pr).all() and (Pr <= 1).all()):
         raise ValueError("All values in Pr must be in the range [0, 1]")
-    if not np.isclose(Pr.all(), 1):
+    if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
     return likelihood(x, n, P) * Pr
