d544edce8ec21aa7b215da361d28dfaa445900cc
AmineNeifer <amineneifer2000@gmail.com>
2020-10-31 09:47:07 +0000

    shape of Pr and P must be the same

--- math/0x07-bayesian_prob/2-marginal.py
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -34,7 +34,7 @@ def marginal(x, n, P, Pr):
         raise TypeError("P must be a 1D numpy.ndarray")
     if not ((0 <= P).all() and (P <= 1).all()):
         raise ValueError("All values in P must be in the range [0, 1]")
-    if not isinstance(Pr, np.ndarray) or len(P.shape) != len(Pr.shape):
+    if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if not ((0 <= Pr).all() and (Pr <= 1).all()):
         raise ValueError("All values in Pr must be in the range [0, 1]")
