9dc753e584bdbc0813e120f11728099dfb489fb0
AmineNeifer <amineneifer2000@gmail.com>
2020-11-05 12:47:36 +0000

    ih

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -13,4 +13,13 @@ def initialize(X, k):
 def kmeans(X, k, iterations=1000):
     """ kmeans algorithm"""
     cent = initialize(X, k)
+    assigned_cent = np.zeros((X.shape[0],), dtype=int)
+    for i in range(X.shape[0]):
+        distance = np.linalg.norm(X[i] - cent, axis=1)
+        assigned_cent[i] = np.argmin(distance)
+    for i in range(cent.shape[0]):
+        if X[np.where(assigned_cent == i)].size != 0:
+            cent[i] = X[np.where(assigned_cent == i)].mean(axis=0)
+    return cent, assigned_cent
+
     
