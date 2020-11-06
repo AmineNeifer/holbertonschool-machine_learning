730873e74b1e7a67b23474d06f68e40459c92f9d
AmineNeifer <amineneifer2000@gmail.com>
2020-11-06 23:05:06 +0000

    added importing lines

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -3,7 +3,8 @@
 
 """ contains a funct that returns optimum"""
 import numpy as np
-
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """optimum k"""
