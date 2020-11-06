99161693170d32b66389ef288a9982b972b5f257
AmineNeifer <amineneifer2000@gmail.com>
2020-11-06 22:59:18 +0000

    grades

---
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+""" contains a funct that returns optimum"""
+import numpy as np
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """optimum k"""
+    if not isinstance(X, np.ndarray):
+        return None, None
+    if len(X.shape) != 2:
+        return None, None
+    if not isinstance(kmin, int) or kmin < 0:
+        return None, None
+    if not isinstance(kmax, int) or kmin < 0:
+        return None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
