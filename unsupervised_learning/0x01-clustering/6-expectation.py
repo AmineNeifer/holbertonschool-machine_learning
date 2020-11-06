ea839febef4b9a9bac197932ba3b7721b06cc20f
AmineNeifer <amineneifer2000@gmail.com>
2020-11-06 21:47:42 +0000

    I'll complete it later

---
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+""" expectation"""
+import numpy as np
+pdf = __import__('5-pdf').pdf
+
+
+def expectation(X, pi, m, S):
+    """  EM algorithm for a GMM"""
+    if not isinstance(X, np.ndarray):
+        return None, None
+    if len(X.shape) != 2:
+        return None, None
+    n, d = X.shape
+    if not isinstance(pi, np.ndarray):
+        return None, None
+    if len(pi.shape) != 1:
+        return None, None
+    k = pi.shape[0]
+    if not np.isclose(pi.sum(), 1):
+        return None, None
+    if not isinstance(m, np.ndarray):
+        return None, None
+    if len(m.shape) != 2 or m.shape != (k, d):
+        return None, None
+    if not isinstance(S, np.ndarray):
+        return None, None
+    if len(S.shape) != 3 or S.shape != (k, d, d):
+        return None, None
+    np.seterr(all="ignore")   
