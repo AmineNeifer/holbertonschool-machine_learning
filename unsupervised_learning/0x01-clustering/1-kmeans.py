728a105263d3619b53910a9628408456a0c170c6
AmineNeifer <amineneifer2000@gmail.com>
2020-11-06 11:14:26 +0000

    correcting intialize funct

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -7,33 +7,48 @@ import matplotlib.pyplot as plt
 
 def initialize(X, k):
     """ initializes cluster centroids for K-means"""
+    if not isinstance(X, np.ndarray):
+        return None
+    if len(X.shape) != 2:
+        return None
+    if not isinstance(k, int):
+        return None
+    if k <= 0:
+        return None
     n, d = X.shape
     high = np.amax(X, axis=0)
     low = np.amin(X, axis=0)
     cent = np.random.uniform(low, high, (k, d))
     return cent
+
+
 def closest_centroid(points, centroids):
-    """returns an array containing the index to the nearest centroid for each point"""
+    """
+    returns an array containing the idx to the nearest centroid for each nt
+    """
     distances = np.sqrt(((points - centroids[:, np.newaxis])**2).sum(axis=2))
     return np.argmin(distances, axis=0)
 
+
 def move_centroids(points, closest, centroids):
     """returns the new centroids assigned from the points closest to them"""
-    return np.array([points[closest==k].mean(axis=0) for k in range(centroids.shape[0])])
+    return np.array([points[closest == k].mean(axis=0)
+                     for k in range(centroids.shape[0])])
+
 
 def kmeans(X, k, iterations=1000):
     """ kmeans algorithm"""
-    cent = initialize(X, k).copy()
+    c = initialize(X, k)
     i = 0
-    
+
     for i in range(10):
         closest = closest_centroid(X, cent)
         if np.isnan(cent).any():
             cent = initialize(X, k).copy()
         else:
             cent = move_centroids(X, closest, cent)
-        #plt.scatter(X[:, 0], X[:, 1], s=10, c=closest)
-        #plt.scatter(cent[:, 0], cent[:, 1], s=50, marker='*', c=list(range(5)))
-        #plt.show()
+        plt.scatter(X[:, 0], X[:, 1], s=10, c=closest)
+        plt.scatter(cent[:, 0], cent[:, 1], s=50, marker='*', c=list(range(5)))
+        plt.show()
 
-    return cent, closest 
+    return cent, closest
