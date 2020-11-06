62097342f4414bdfffdf45e23efa9935a11857a4
AmineNeifer <amineneifer2000@gmail.com>
2020-11-06 23:04:02 +0000

    just checks

---
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+""" bic funct"""
+import numpy as np
+
+
+def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
+    """ finds best number of clusters for GMM"""
+    f not isinstance(X, np.ndarray):
+        return None, None
+    if len(X.shape) != 2:
+        return None, None, None, None
+    if not isinstance(kmin, int) or kmin < 0:
+        return None, None, None, None
+    if not isinstance(kmax, int) or kmin < 0:
+        return None, None, None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None, None, None
+    if not isinstance(tol, float) or tol < 0:
+        return None, None, None, None
+    if not isinstance(verbose, bool):
+        return None, None, None, None
