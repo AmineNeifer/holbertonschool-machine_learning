f11bb6d171dd221c93dce4bebf8967a0d5ffc6dd
AmineNeifer <amineneifer2000@gmail.com>
2020-11-06 23:08:37 +0000

    works

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -8,7 +8,7 @@ expectation_maximization = __import__('8-EM').expectation_maximization
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """ finds best number of clusters for GMM"""
-    f not isinstance(X, np.ndarray):
+    if not isinstance(X, np.ndarray):
         return None, None
     if len(X.shape) != 2:
         return None, None, None, None
