160f06e652cb7d34af182e925498c79e9e711453
AmineNeifer <amineneifer2000@gmail.com>
2020-11-13 13:49:57 +0000

    oh mygod

---
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+
+""" contains forward algo for hmm"""
+import numpy as np
+
+
+def forward(Observation, Emission, Transition, Initial):
+    """ 
+    performs the forward algorithm for a hidden markov model
+    
+    A = Transition
+    B = Emission
+    Pi = Initial
+    O = Observation
+
+    """
+    if not isinstance(Observation, np.ndarray):
+        return None, None
+    if len(Observation.shape) != 1:
+        return None, None
+    T = Observation.shape[0]
+    if not isinstance(Emission, np.ndarray):
+        return None, None
+    if len(Emission.shape) != 2:
+        return None, None
+    N, M = Emission.shape
+    if not isinstance(Transition, np.ndarray):
+        return None, None
+    if Transition.shape != (N, N):
+        return None, None
+    if not isinstance(Initial, np.ndarray):
+        return None, None
+    if Initial.shape != (N, 1):
+        return None, None
+    F = np.zeros((N, T))
+    for s in range(N):
+        F[s, 0] = Initial[s] * Emission[s, Observation[0]]
+    for t in range(1, T):
+        obs_index = Observation[t]
+        for s in range(N):
+            F[s, t] = np.dot(F[:, t-1], Transition[:, s]) * Emission[s, Observation[t]]
+    P = np.sum(F[:, T-1], axis=0)
+    return P , F
