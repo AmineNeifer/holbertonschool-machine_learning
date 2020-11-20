1d13266d27629236177000a4f0e3137d13f17e91
AmineNeifer <amineneifer2000@gmail.com>
2020-11-20 14:46:48 +0000

    just uploading to save the work

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+""" contains Bayesian Optimization"""
+import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """Performs Bayesian optimization on a noiseless 1D GP"""
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """ Class constructor"""
+        bo = bounds
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.xsi = xsi
+        self.X_s = np.linspace(bo[0], bo[1], ac_samples).reshape((-1, 1))
+        self.minimize = minimize
+
+ 
+    def acquisition(self):
+        """calculates the next best sample location"""
+        mu, sigma = self.gp.predict(self.X_s)
+        mu_sample_opt = np.max(mu)
+
+        imp = mu - mu_sample_opt - self.xsi
+        Z = imp / sigma
+        ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+        ei[sigma == 0.0] = 0.0
+
+        return None, ei
