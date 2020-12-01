fcff8fd76c92a3b87321a286d5722818a3459086
AmineNeifer <amineneifer2000@gmail.com>
2020-12-01 10:53:46 +0000

    task 0

--- supervised_learning/0x0D-RNNs/0-rnn_cell.py
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -7,18 +7,29 @@ import numpy as np
 
 
 class RNNCell:
-	def __init_(self, i, h, o):
-		"""
-		Class constructor:
+    def __init__(self, i, h, o):
+        """
+        Class constructor:
 
-		@i is the dimensionality of the data
-		@h is the dimensionality of the hidden state
-		@o is the dimensionality of the outputs
-		- Creates the public instance attributes @Wh, @Wy, @bh, @by
-			@Wh and @bh are for the concatenated hidden state and input data
-			@Wy and @by are for the output
-		"""
-		self.Wh = np.random.normal(-np.sqrt(1./i), np.sqrt(1./i), (h, i))
-		self.bh = 0
-		self.Wy = np.random.normal(-np.sqrt(1./i), np.sqrt(1./i), (h, i))
-		self.by = 0
+        @i is the dimensionality of the data
+        @h is the dimensionality of the hidden state
+        @o is the dimensionality of the outputs
+        - Creates the public instance attributes @Wh, @Wy, @bh, @by
+            @Wh and @bh are for the concatenated hidden state and input data
+            @Wy and @by are for the output
+        """
+        self.Wh = np.random.normal(size=(h + i, h))
+        self.bh = np.zeros((1, h))
+        self.Wy = np.random.normal(size=(h, o))
+        self.by = np.zeros((1, o))
+
+    def softmax(self, x):
+        """Compute softmax values for each sets of scores in x."""
+        return np.exp(x)/np.sum(np.exp(x), axis=0)
+
+    def forward(self, h_prev, x_t):
+        """ feed forward algo for RNN"""
+        conc = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(conc.dot(self.Wh) + self.bh)
+        y = self.softmax(h_next.dot(self.Wy) + self.by)
+        return h_next, y
