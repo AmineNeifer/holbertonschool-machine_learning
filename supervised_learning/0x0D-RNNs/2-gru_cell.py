556034680e14973b666ade98da8ffe7c48ef1b26
AmineNeifer <amineneifer2000@gmail.com>
2020-12-03 21:13:18 +0000

    pycodestyle

--- supervised_learning/0x0D-RNNs/2-gru_cell.py
+++ supervised_learning/0x0D-RNNs/2-gru_cell.py
@@ -14,7 +14,8 @@ class GRUCell:
         @i is the dimensionality of the data
         @h is the dimensionality of the hidden state
         @o is the dimensionality of the outputs
-        - Creates the public instance attributes that represent the weights and biases of the cell
+        - Creates the public instance attributes that represent
+        the weights and biases of the cell
             @Wz and @bz are for the update gate
             @Wr and @br are for the reset gate
             @Wh and @bh are for the intermediate hidden state
@@ -28,25 +29,28 @@ class GRUCell:
         self.br = np.zeros((1, h))
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
+
     def sigmoid(self, x):
         """sigmoid fucntion"""
         return 1 / (1 + np.exp(-x))
+
     def softmax(self, x):
         """Compute softmax values for each sets of scores in x."""
-        return np.exp(x)/np.sum(np.exp(x), axis=1, keepdims=True)
+        return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+
     def forward(self, h_prev, x_t):
         """
         Performs forward propagation for one time step
             @x_t is a np.ndarray (m, i) contains the data input for the cell
                 m is the batche size for the data
             @h_prev is a np.ndarray (m, h) contains the previous hidden state
-            
+
         Returns: h_next, y
             @h_next is the next hidden state
             @y is the output of the cell
         """
         comb = np.concatenate((h_prev, x_t), axis=1)
-        
+
         z = self.sigmoid(comb.dot(self.Wz) + self.bz)
         r = self.sigmoid(comb.dot(self.Wr) + self.br)
         comb = np.concatenate((h_prev * r, x_t), axis=1)
