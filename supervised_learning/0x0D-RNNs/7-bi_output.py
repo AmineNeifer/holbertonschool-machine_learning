9b1bdd52af29b278c82551e9dc647a17214cadcc
AmineNeifer <amineneifer2000@gmail.com>
2020-12-03 18:31:57 +0000

    bi rnn

---
+++ supervised_learning/0x0D-RNNs/7-bi_output.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+"""
+Contains class BidirectionalCell that represents a bidirectional cell RNN
+"""
+import numpy as np
+
+
+class BidirectionalCell:
+    def __init__(self, i, h, o):
+        """
+        Class constructor:
+
+        @i is the dimensionality of the data
+        @h is the dimensionality of the hidden state
+        @o is the dimensionality of the outputs
+        - Creates the public instance attributes
+            @Whf and @bhf are for the hidden states in the forward direction
+            @Whb and @bhb are for the hidden states in the backward direction
+            @Wy and @by are for the outputs
+        """
+        self.Whf = np.random.normal(size=(h + i, h))
+        self.bhf = np.zeros((1, h))
+        self.Whb = np.random.normal(size=(h + i, h))
+        self.bhb = np.zeros((1, h))
+        self.Wy = np.random.normal(size=(2 * h, o))
+        self.by = np.zeros((1, o))
+
+    def softmax(self, x):
+        """Compute softmax values for each sets of scores in x."""
+        return np.exp(x)/np.sum(np.exp(x), axis=1, keepdims=True)
+
+    def forward(self, h_prev, x_t):
+        """ feed forward algo for bidirectional RNN"""
+        conc = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(conc.dot(self.Whf) + self.bhf)
+        #y = self.softmax(h_next.dot(self.Wy) + self.by)
+        return h_next
+    def backward(self, h_next, x_t):
+        """ feed backward algo for bidirectional RNN"""
+        conc = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(conc.dot(self.Whb) + self.bhb)
+        return h_prev
+
+    def output(self, H):
+        """ Calculates all outputs for the RNN"""
+        T = H.shape[0]
+        y = []
+        for t in range(T):
+            y.append(self.softmax(H[t].dot(self.Wy) + self.by))
+        return np.array(y)
