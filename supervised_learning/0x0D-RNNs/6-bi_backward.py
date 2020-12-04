db162e09ed6db85b607c63b6f74290a6ec8f42f4
AmineNeifer <amineneifer2000@gmail.com>
2020-12-04 19:16:40 +0000

    typo

--- supervised_learning/0x0D-RNNs/6-bi_backward.py
+++ supervised_learning/0x0D-RNNs/6-bi_backward.py
@@ -41,4 +41,4 @@ class BidirectionalCell:
         """ feed backward algo for bidirectional RNN"""
         conc = np.concatenate((h_next, x_t), axis=1)
         h_prev = np.tanh(conc.dot(self.Whb) + self.bhb)
-        return h_prevbidirectional cell RNN
+        return h_prev
