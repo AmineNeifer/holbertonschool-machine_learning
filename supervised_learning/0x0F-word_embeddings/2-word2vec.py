d96d99dce141d4103124e9e74c3a0991962290eb
AmineNeifer <amineneifer2000@gmail.com>
2020-12-23 17:22:55 +0000

    sg not cbow

--- supervised_learning/0x0F-word_embeddings/2-word2vec.py
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -7,13 +7,14 @@ from gensim.models import Word2Vec
 def word2vec_model(sentences, size=100, min_count=5, window=5,
                    negative=5, cbow=True, iterations=5, seed=0, workers=1):
     """ Creates and trains gensim word2vec model"""
+    sg = not cbow
     model = Word2Vec(
         sentences=sentences,
         size=size,
         min_count=min_count,
         window=window,
         negative=negative,
-        sg=cbow,
+        sg=sg,
         iter=iterations,
         seed=seed,
         workers=workers)
