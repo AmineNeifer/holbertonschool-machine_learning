774be79f88bed3ec2ee10e824de60da91fefb820
AmineNeifer <amineneifer2000@gmail.com>
2021-01-14 21:08:50 +0000

    validate -> validation

--- supervised_learning/0x12-transformer_apps/0-dataset.py
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -13,7 +13,7 @@ class Dataset:
         """Constructor"""
         self.data_train, self.data_valid = tfds.load(
             'ted_hrlr_translate/pt_to_en',
-            split=['train', 'validate'],
+            split=['train', 'validation'],
             as_supervised=True)
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train)
