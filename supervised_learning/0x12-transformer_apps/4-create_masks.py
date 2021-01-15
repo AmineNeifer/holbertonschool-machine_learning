a7e723b56cd5b9a8ea83b8df0f275f8cebe436b0
AmineNeifer <amineneifer2000@gmail.com>
2021-01-15 18:13:54 +0000

    creates masks

---
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+""" contains create_masks funct"""
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+
+def create_masks(inputs, target):
+    """creates all masks for training/validation"""
+    batch_size, seq_len_out = target.shape
+    encoder_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)[
+        :, tf.newaxis, tf.newaxis, :]
+    mask = 1 - \
+        tf.linalg.band_part(
+            tf.ones(
+                (batch_size, 1, seq_len_out, seq_len_out)), -1, 0)
+    decoder_mask = tf.cast(tf.math.equal(target, 0), tf.float32)[
+        :, tf.newaxis, tf.newaxis, :]
+    return encoder_mask, mask, decoder_mask
