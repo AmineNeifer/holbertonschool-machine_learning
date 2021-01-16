89658d673f195ede9a1d6be9ddb8044ef171828d
AmineNeifer <amineneifer2000@gmail.com>
2021-01-16 19:36:10 +0000

    changed location of the tf.reshape line

--- supervised_learning/0x11-attention/2-rnn_decoder.py
+++ supervised_learning/0x11-attention/2-rnn_decoder.py
@@ -40,6 +40,7 @@ class RNNDecoder(tf.keras.layers.Layer):
 
         concat = tf.concat([tf.expand_dims(context, 1), x], -1)
         outputs, hidden = self.gru(concat)
+        outputs = tf.reshape(outputs, (batch, -1))
         output = self.F(outputs)
 
-        return tf.reshape(output, (batch, -1)), hidden
+        return output, hidden
