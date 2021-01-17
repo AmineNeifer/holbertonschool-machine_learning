c107183f99eb129504e7d20df9699e5648641ccc
AmineNeifer <amineneifer2000@gmail.com>
2021-01-17 15:03:49 +0000

    scaled dot product

---
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+""" contains the scaled dot product attention"""
+import tensorflow as tf
+
+
+def sdp_attention(q, k, v, mask=None):
+    """
+    Calculate scaled dot product attention
+
+    Returns:
+        output, attention_weights
+    """
+
+    # (..., seq_len_q, seq_len_k)
+    matmul_qk = tf.matmul(q, k, transpose_b=True)
+
+    # scale matmul_qk
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+
+    output = tf.matmul(attention_weights, v)
+
+    return output, attention_weights
