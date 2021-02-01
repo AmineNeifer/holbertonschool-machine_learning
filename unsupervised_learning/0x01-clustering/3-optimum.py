927a91370837641c42076d89afeb3f74743b818a
AmineNeifer <amineneifer2000@gmail.com>
2021-02-01 16:18:11 +0100

    optimum k

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -6,6 +6,7 @@ import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 variance = __import__('2-variance').variance
 
+
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """optimum k"""
     if not isinstance(X, np.ndarray):
@@ -18,3 +19,14 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         return None, None
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
+    if kmax - kmin <= 2:
+        return None, None
+    results = []
+    d_vars = []
+    for i in range(kmin, kmax + 1):
+        c, clss = kmeans(X, i, iterations)
+        results.append((c, clss))
+        d_vars.append(variance(X, c))
+    for i in range(len(d_vars)):
+        d_vars[i] = variance(X, results[0][0]) - d_vars[i]
+    return results, d_vars
