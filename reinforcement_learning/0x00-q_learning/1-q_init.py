927516184066b328edb79fc0eff3971a005fbfc7
Amine Neifer <amineneifer2000@gmail.com>
2021-02-09 21:37:53 +0100

    initialization of Q-table

---
+++ reinforcement_learning/0x00-q_learning/1-q_init.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+"""contains q_init function """
+
+import numpy as np
+
+
+def q_init(env):
+    """ that initializes the Q-table"""
+    action_space_size = env.action_space.n
+    state_space_size = env.observation_space.n
+    q_table = np.zeros((state_space_size, action_space_size))
+    return q_table
