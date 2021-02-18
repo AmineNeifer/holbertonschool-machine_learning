f7d1cdee50e681f5a5e86082d814aec349d7232a
Amine Neifer <amineneifer2000@gmail.com>
2021-02-18 21:01:10 +0100

    readme file with training and playing function

---
+++ reinforcement_learning/0x01-deep_q_learning/play.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+import numpy as np
+import gym
+from PIL import Image
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute
+from keras.optimizers import Adam
+import keras.backend as K
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+from rl.core import Processor
+
+
+INPUT_SHAPE = (84, 84)
+WINDOW_LENGTH = 4
+
+
+class AtariProcessor(Processor):
+    def process_observation(self, observation):
+        assert observation.ndim == 3  # (height, width, channel)
+        img = Image.fromarray(observation)
+        # resize and convert to grayscale
+        img = img.resize(INPUT_SHAPE).convert('L')
+        processed_observation = np.array(img)
+        assert processed_observation.shape == INPUT_SHAPE
+        # save storage in experience memory
+        return processed_observation.astype('uint8')
+
+    def process_state_batch(self, batch):
+        # We could perform this processing step in `process_observation`.
+        # In this case, however, we would need to store a `float32` array
+        # instead, which is 4x more memory intensive than
+        # an `uint8` array. This matters if we store 1M observations.
+        processed_batch = batch.astype('float32') / 255.
+        return processed_batch
+
+    def process_reward(self, reward):
+        return np.clip(reward, -1., 1.)
+
+
+# Get the environment and extract the number of actions.
+env = gym.make("Breakout-v0")
+np.random.seed(123)
+env.seed(123)
+nb_actions = env.action_space.n
+
+# Next, we build our model. We use the same model
+# that was described by Mnih et al. (2015).
+input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
+model = Sequential()
+if K.image_dim_ordering() == 'tf':
+    # (width, height, channels)
+    model.add(Permute((2, 3, 1), input_shape=input_shape))
+elif K.image_dim_ordering() == 'th':
+    # (channels, width, height)
+    model.add(Permute((1, 2, 3), input_shape=input_shape))
+else:
+    raise RuntimeError('Unknown image_dim_ordering.')
+model.add(Convolution2D(32, (8, 8), strides=(4, 4)))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, (4, 4), strides=(2, 2)))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, (3, 3), strides=(1, 1)))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+print(model.summary())
+
+# Finally, we configure and compile our agent.
+# You can use every built-in Keras optimizer and
+# even the metrics!
+memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
+processor = AtariProcessor()
+
+policy = LinearAnnealedPolicy(
+    EpsGreedyQPolicy(),
+    attr='eps',
+    value_max=1.,
+    value_min=.1,
+    value_test=.05,
+    nb_steps=1000000)
+
+
+dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy,
+               memory=memory, processor=processor, nb_steps_warmup=50000,
+               gamma=.99, target_model_update=10000, train_interval=4,
+               delta_clip=1.)
+dqn.compile(Adam(lr=.00025), metrics=['mae'])
+
+
+weights_filename = 'policy.h5'
+# since we did some training before, we just upload the weights.
+dqn.load_weights(weights_filename)
+
+# Finally, evaluate our algorithm for 10 episodes.
+dqn.test(env, nb_episodes=10, visualize=True)
