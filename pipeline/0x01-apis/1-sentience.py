1b058fcbe0c5bb606cb29b4777f023d1ed71e1cb
Amine Neifer <amineneifer2000@gmail.com>
2021-03-18 13:23:29 +0100

    task 1

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 
+""" Contains sentientPlanets function"""
 import requests
 
+
 def sentientPlanets():
+    """
+    Returns the list of names of the home planets of all
+    sentient species, using the Swapi API.
+    """
     url = "https://swapi-api.hbtn.io/api/species"
     lista = []
-    while True:        
+    while True:
         r = requests.get(url).json()
         for value in r["results"]:
             if value["designation"] == "sentient":
-                try: 
-                    lista.append(requests.get(value["homeworld"]).json()["name"])
-                except:
+                try:
+                    lista.append(
+                        requests.get(
+                            value["homeworld"]).json()["name"])
+                except BaseException:
                     pass
         url = r["next"]
         if url is None:
