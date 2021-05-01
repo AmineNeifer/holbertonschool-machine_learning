a1303b09bb7dabe4d42ed16ec63a56bdfaebd442
Amine Neifer <amineneifer2000@gmail.com>
2021-05-01 17:21:39 +0100

    idk

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -14,7 +14,7 @@ def sentientPlanets():
     while True:
         r = requests.get(url).json()
         for value in r["results"]:
-            if value["designation"] == "sentient":
+            if value["designation"] == "sentient" or value['classification'] == 'sentient':
                 try:
                     lista.append(
                         requests.get(
