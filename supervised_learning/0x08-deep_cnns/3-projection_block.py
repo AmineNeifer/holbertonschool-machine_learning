718bce90a6e94ad11a55894b8f4cc435d75ba1f9
Neifer <52834823+AmineNeifer@users.noreply.github.com>
2021-10-27 16:36:43 +0100

    change func

--- supervised_learning/0x08-deep_cnns/3-projection_block.py
+++ supervised_learning/0x08-deep_cnns/3-projection_block.py
@@ -14,13 +14,13 @@ def projection_block(A_prev, filters, s=2):
         strides=s,
         padding='same', kernel_initializer="he_normal")(A_prev)
     conv_1x1 = K.layers.BatchNormalization()(conv_1x1)
-    conv_1x1 = K.layers.Activation('relu')(conv_1x1)
+    conv_1x1 = K.layers.ReLU()(conv_1x1)
     conv_3x3 = K.layers.Conv2D(
         filters=F3,
         kernel_size=3,
         padding='same', kernel_initializer="he_normal")(conv_1x1)
     conv_3x3 = K.layers.BatchNormalization()(conv_3x3)
-    conv_3x3 = K.layers.Activation('relu')(conv_3x3)
+    conv_3x3 = K.layers.ReLU()(conv_3x3)
     conv_1x1 = K.layers.Conv2D(
         filters=F12,
         kernel_size=1,
@@ -34,5 +34,5 @@ def projection_block(A_prev, filters, s=2):
         padding='same', kernel_initializer="he_normal")(A_prev)
     shortcut = K.layers.BatchNormalization()(shortcut)
     added = K.layers.Add()([conv_1x1, shortcut])
-    output = K.layers.Activation('relu')(added)
+    output = K.layers.ReLU()(added)
     return output
