e660769467159adc8936722498d75d2100f87db4
Neifer <52834823+AmineNeifer@users.noreply.github.com>
2021-11-18 14:30:21 +0100

    task 9

--- supervised_learning/0x0C-neural_style_transfer/9-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/9-neural_style.py
@@ -182,50 +182,50 @@ class NST:
         return grads, J, J_content, J_style
 
 
-def generate_image(self, iterations=1000, step=None,
-                   lr=0.01, beta1=0.9, beta2=0.99):
-    if type(iterations) is not int:
-        raise TypeError('iterations must be an integer')
-    if iterations < 1:
-        raise ValueError('iterations must be positive')
-    if step is not None and type(step) is not int:
-        raise TypeError('step must be an integer')
-    if step is not None and not 0 < step < iterations:
-        raise ValueError('step must be positive and less than iterations')
-    if type(lr) is not float and type(lr) is not int:
-        raise TypeError('lr must be a number')
-    if lr <= 0:
-        raise ValueError('lr must be positive')
-    if type(beta1) is not float:
-        raise TypeError('beta1 must be a float')
-    if not 0 <= beta1 <= 1:
-        raise ValueError('beta1 must be in the range [0, 1]')
-    if type(beta2) is not float:
-        raise TypeError('beta2 must be a float')
-    if not 0 <= beta2 <= 1:
-        raise ValueError('beta2 must be in the range [0, 1]')
-    generated_image = tf.Variable(self.content_image)
-    optimizer = tf.keras.optimizers.Adam(lr, beta1, beta2)
-    # optimizer = tf.train.AdamOptimizer(lr, beta1, beta2) tf-V1
-    best_cost = np.inf
-    best_image = None
-    for i in range(iterations):
-        grads, J, J_content, J_style = self.compute_grads(generated_image)
-        if step is not None and i % step == 0:
-            print("Cost at iteration {}: {}, content {}, style {}".format(
-                i, J, J_content, J_style))
+    def generate_image(self, iterations=1000, step=None,
+                    lr=0.01, beta1=0.9, beta2=0.99):
+        if type(iterations) is not int:
+            raise TypeError('iterations must be an integer')
+        if iterations < 1:
+            raise ValueError('iterations must be positive')
+        if step is not None and type(step) is not int:
+            raise TypeError('step must be an integer')
+        if step is not None and not 0 < step < iterations:
+            raise ValueError('step must be positive and less than iterations')
+        if type(lr) is not float and type(lr) is not int:
+            raise TypeError('lr must be a number')
+        if lr <= 0:
+            raise ValueError('lr must be positive')
+        if type(beta1) is not float:
+            raise TypeError('beta1 must be a float')
+        if not 0 <= beta1 <= 1:
+            raise ValueError('beta1 must be in the range [0, 1]')
+        if type(beta2) is not float:
+            raise TypeError('beta2 must be a float')
+        if not 0 <= beta2 <= 1:
+            raise ValueError('beta2 must be in the range [0, 1]')
+        generated_image = tf.Variable(self.content_image)
+        optimizer = tf.keras.optimizers.Adam(lr, beta1, beta2)
+        # optimizer = tf.train.AdamOptimizer(lr, beta1, beta2) tf-V1
+        best_cost = np.inf
+        best_image = None
+        for i in range(iterations):
+            grads, J, J_content, J_style = self.compute_grads(generated_image)
+            if step is not None and i % step == 0:
+                print("Cost at iteration {}: {}, content {}, style {}".format(
+                    i, J, J_content, J_style))
+            if J < best_cost:
+                best_cost = J.numpy()
+                best_image = generated_image.numpy()[0]
+            optimizer.apply_gradients([(grads, generated_image)])
+            generated_image.assign(tf.clip_by_value(
+                generated_image, clip_value_min=0.0, clip_value_max=1.0))
+
+        _, J, J_content, J_style = self.compute_grads(generated_image)
         if J < best_cost:
             best_cost = J.numpy()
             best_image = generated_image.numpy()[0]
-        optimizer.apply_gradients([(grads, generated_image)])
-        generated_image.assign(tf.clip_by_value(
-            generated_image, clip_value_min=0.0, clip_value_max=1.0))
-
-    _, J, J_content, J_style = self.compute_grads(generated_image)
-    if J < best_cost:
-        best_cost = J.numpy()
-        best_image = generated_image.numpy()[0]
-    if step is not None:
-        print("Cost at iteration {}: {}, content {}, style {}".format(
-            iterations, J, J_content, J_style))
-    return best_image, best_cost
+        if step is not None:
+            print("Cost at iteration {}: {}, content {}, style {}".format(
+                iterations, J, J_content, J_style))
+        return best_image, best_cost
