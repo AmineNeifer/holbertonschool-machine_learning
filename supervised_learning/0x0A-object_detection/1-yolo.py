7fbc8647465083dba8af91a247b6658752a38dab
Neifer <52834823+AmineNeifer@users.noreply.github.com>
2021-11-19 09:58:00 +0100

    pycodestyle

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -3,16 +3,19 @@
 import numpy as np
 import tensorflow.keras as K
 
+
 class Yolo:
     """Class to perform the Yolo algorithm on image data"""
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """initializes the Yolo class"""
-        self.class_t = class_t # class score threshold
-        self.nms_t = nms_t # non max suppression threshold
-        self.model = K.models.load_model(model_path) # keras darknet model
-        self.anchors = anchors # anchor boxes
+        self.class_t = class_t  # class score threshold
+        self.nms_t = nms_t  # non max suppression threshold
+        self.model = K.models.load_model(model_path)  # keras darknet model
+        self.anchors = anchors  # anchor boxes
         with open(classes_path) as f:
-            self.class_names = [class_name.strip() for class_name in f.readlines()]
+            self.class_names = [class_name.strip()
+                                for class_name in f.readlines()]
 
     def process_outputs(self, outputs, image_size):
         """processes the outputs of the model"""
@@ -23,15 +26,17 @@ class Yolo:
         for i, output in enumerate(outputs):
             anchors = self.anchors[i]
             g_h, g_w = output.shape[:2]
-            
+
             t_xy = output[..., :2]
             t_wh = output[..., 2:4]
-            box_confidence = np.expand_dims(1/(1 + np.exp(-output[..., 4])), axis=-1)
+            box_confidence = np.expand_dims(
+                1/(1 + np.exp(-output[..., 4])), axis=-1)
             box_class_prob = 1/(1 + np.exp(-output[..., 5:]))
 
             b_wh = anchors * np.exp(t_wh)
             b_wh = b_wh / self.model.inputs[0].shape.as_list()[1:3]
-            grid = np.tile(np.indices((g_w, g_h)).T, anchors.shape[0]).reshape((g_h, g_w) + anchors.shape)
+            grid = np.tile(np.indices((g_w, g_h)).T, anchors.shape[0]).reshape(
+                (g_h, g_w) + anchors.shape)
             b_xy = (1/(1 + np.exp(-t_xy)) + grid) / [g_w, g_h]
             b_xy1 = b_xy - (b_wh / 2)
             b_xy2 = b_xy + (b_wh / 2)
